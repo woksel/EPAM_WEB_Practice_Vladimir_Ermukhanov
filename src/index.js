36ee991b2205a757dc6ac71d570f375d6d30d09e
Vladimir Ermukhanov <ermuhanoff@gmail.com>
2020-06-30 15:49:05 +0400

    feat: webpack plugins

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 function main() {
-
+  console.log("Hello world");
 }
 window.onload = main;
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
+import Html from "html-webpack-plugin";
+import Clean from "clean-webpack-plugin";
+import path from "path";
+
 module.exports = {
-    mode: 'development',
-    entry: 'src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: 'dist'
-    }
-}
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "src",
+  output: {
+    filename: "[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new Html({
+      template: "index.html",
+    }),
+    new Clean(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        //use: ["css-loader", "style-loader"],
+        use: ["style-loader"],
+      },
+    ],
+  },
+};
